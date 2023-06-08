022b09544d8b9bfeca878f8a850a2ed83219e42b
saurabhjaykar1602 <jaykarsaurabh97@gmail.com>
2023-06-08 23:40:34 +0530

    feature - with the help of usestate and hook i created simple a text converter (Lowercase to Upercase) With the help of onHandleChange event for listening and handleClick Event for Value UpperCassing

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,13 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
+
 
 function TextForm(props) {
+    const handleUpClick = ()=>{
+        console.log('Upeercase was clicked'+text)
+        let newText = text.toUpperCase();
+        setText(newText)
+    }
+
+
+    const handleOnChange = (e)=>{
+        console.log('Upeercase was clicked')
+        setText(e.target.value)
+    }
+    const [text, setText] = useState('Enter Text here');
+    // text = "newt text"; // Wrong way to change the state
+    //setText ("new text");  //Correct way to change the state
     return (
         <div>
-            <h1>{props.heading}</h1>
-            <div class="mb-3">
-
-                <textarea class="form-control" id="myBox" rows="8"></textarea>
+            <h1>{props.heading} </h1>
+            <div className="mb-3">
+                <textarea className="form-control" id="myBox" rows="8" value={text} onChange={handleOnChange}></textarea>
             </div>
+            <button className="btn btn-primary" onClick={handleUpClick}>Convert to Uppercase</button>
         </div>
 
     )
