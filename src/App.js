cfba2110b0a0fae7e3839bbad90e0297de22d15e
saurabhjaykar1602 <jaykarsaurabh97@gmail.com>
2023-06-11 08:59:23 +0530

    new feature - dark mode enable in about page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import './App.css';
-import About from './components/About';
+// import About from './components/About';
 import Navbar from './components/Navbar';
-// import TextForm from './components/TextForm';
+import TextForm from './components/TextForm';
 
 function App() {
   return (
     <>
       <Navbar tital="TextMASTER" Abouttext="About Us" />
       <div className="container my-3">
-        {/* <TextForm  heading="Enter The Text To Analyze Below"/> */}
-        <About/>
+        <TextForm heading="Enter The Text To Analyze Below"/>
+        {/* <About/> */}
       </div>
     </>
   );
